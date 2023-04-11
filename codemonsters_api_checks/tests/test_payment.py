4ef3cca2b82780a44e27de42b156fb9d8732ee38
Tihomira Kostova <t.feralieva@gmail.com>
2023-04-12 00:38:55 +0300

    Fix wrong test location

--- codemonsters_api_checks/tests/test_payment.py
+++ codemonsters_api_checks/tests/test_payment.py
@@ -2,10 +2,16 @@ import pytest
 import requests
 from codemonsters_api_checks.config import config
 
+URL_TRANSACTIONS = f"{config['endpoint']}/payment_transactions"
+
+def get_basic_authentication():
+    username = f"{config['username']}"
+    password = f"{config['password']}"
+    return {'Authorization': 'Basic ' + f'{username}:{password}'}
+
 # Make a request to the payment endpoint with valid data
 def test_valid_payment():
-    url = f"{config['endpoint']}/payment_transactions"
-    headers = {'Authorization': 'Basic 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'}
+    headers = get_basic_authentication()
     data = {
         'amount': '10.00',
         'currency': 'USD',
@@ -13,16 +19,18 @@ def test_valid_payment():
         'card_holder': 'John Doe',
         'exp_date': '1223',
         'cvv': '123',
-        'unique_id': '0e08644635ccb520c2eeb54f33865660'
     }
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(URL_TRANSACTIONS, headers=headers, json=payment_data)
     assert response.status_code == 200
     assert response.json()['status'] == 'approved'
+    assert 'unique_id' in response.json
+    unique_id = response.json()['unique_id']
     
 # Make a request to the payment endpoint with invalid authentication
 def test_invalid_auth_payment():
-    url = f"{config['endpoint']}/payment_transactions"
-    headers = {'Authorization': 'Basic abcdefg1234567890'}
+    username = f"{config['username']}"
+    password = "wrong_password"
+    headers = {'Authorization': 'Basic ' + f'{username}:{password}'}
     data = {
         'amount': '10.00',
         'currency': 'USD',
@@ -30,34 +38,30 @@ def test_invalid_auth_payment():
         'card_holder': 'John Doe',
         'exp_date': '1223',
         'cvv': '123',
-        'unique_id': '0e08644635ccb520c2eeb54f33865668'
     }
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(URL_TRANSACTIONS, headers=headers, json=data)
     assert response.status_code == 401
     
 # Make a request to the void endpoint with a non-existent payment ID
 def test_nonexistent_void():
-    url = f"{config['endpoint']}/payment_transactions"
-    headers = {'Authorization': 'Basic 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'}
+    headers = get_basic_authentication()
     data = {'reference_id': '0e08644635ccb520c2eeb54f33865660'}
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(URL_TRANSACTIONS, headers=headers, json=data)
     assert response.status_code == 422
     assert response.json()['message'] == 'Payment not found'
     
 # Make a request to the void endpoint with an existing void ID
 def test_existing_void():
-    url = f"{config['endpoint']}/payment_transactions"
-    headers = {'Authorization': 'Basic 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'}
-    data = {'reference_id': '0e08644635ccb520c2eeb54f33865660'}
-    response = requests.post(url, headers=headers, json=data)
+    headers = get_basic_authentication()
+    data = {'reference_id': unique_id}
+    response = requests.post(URL_TRANSACTIONS, headers=headers, json=data)
     assert response.status_code == 200
     assert response.json()['message'] == 'Payment already voided'
 
 # Make a request to the void endpoint for nonexisting payment
 def test_nonexisting_payment_void():
-    url = f"{config['endpoint']}/payment_transactions"
-    headers = {'Authorization': 'Basic 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'}
+    headers = get_basic_authentication()
     data = {'reference_id': '0e08644635ccb520c2eeb54f33865668'}
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(URL_TRANSACTIONS, headers=headers, json=data)
     assert response.status_code == 422
     assert response.json()['message'] == 'Payment doesn\'t exist'
